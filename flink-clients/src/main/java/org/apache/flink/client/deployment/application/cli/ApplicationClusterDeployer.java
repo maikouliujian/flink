01b366b42e66cda67a83359522ef3ef45812debf
liujian <liujian1@yidian-inc.com>
2024-02-22 20:53:48 +0800

    【flink源码阅读】 flink job提交入口

--- flink-clients/src/main/java/org/apache/flink/client/deployment/application/cli/ApplicationClusterDeployer.java
+++ flink-clients/src/main/java/org/apache/flink/client/deployment/application/cli/ApplicationClusterDeployer.java
@@ -47,7 +47,7 @@ public class ApplicationClusterDeployer implements ApplicationDeployer {
     public ApplicationClusterDeployer(final ClusterClientServiceLoader clientServiceLoader) {
         this.clientServiceLoader = checkNotNull(clientServiceLoader);
     }
-
+    //todo 通过flink命令提交作业
     public <ClusterID> void run(
             final Configuration configuration,
             final ApplicationConfiguration applicationConfiguration)
@@ -63,7 +63,7 @@ public class ApplicationClusterDeployer implements ApplicationDeployer {
                 clientFactory.createClusterDescriptor(configuration)) {
             final ClusterSpecification clusterSpecification =
                     clientFactory.getClusterSpecification(configuration);
-
+            //todo 通过YarnClusterDescriptor向yarn提交flink作业
             clusterDescriptor.deployApplicationCluster(
                     clusterSpecification, applicationConfiguration);
         }
